02243fb2a783f0514128a6782047a0753b9d4e21
vladyslavnUA <vladyslavn@yahoo.com>
2020-02-13 16:52:30 -0800

    updated wiki files

--- wiki/urls.py
+++ wiki/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from wiki.views import PageListView, PageDetailView
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('', PageListView.as_view(), name='wiki-list-page'),
     path('<str:slug>/', PageDetailView.as_view(), name='wiki-details-page'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('change-password/', auth_views.PasswordChangeView.as_view()),
 ]
